49f43896451cdba85b439400556ee908a34baf8a
Deen John <dj.itbhu@gmail.com>
2019-08-26 04:52:34 +0530

    Add files via upload

--- Webpack learning/webpack.config.js
+++ Webpack learning/webpack.config.js
@@ -3,43 +3,66 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require("webpack");
 var package     = require('./package.json');
 
-module.exports = {
-  entry: {
-    app : './src/index.js',
-    vendor: Object.keys(package.dependencies)
-  },
-  node: {
-    fs: 'empty'
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  target: "web",
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules)/,
-        resolve: { extensions: [".js"] },
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            "plugins": ["@babel/plugin-transform-arrow-functions"]
+module.exports = (env)=>{
+
+  console.log(env);
+  return {
+    devtool: "cheap-source-map",
+    entry: {
+      app : './src/index.js',
+      vendor: Object.keys(package.dependencies)
+    },
+    node: {
+      fs: 'empty'
+    },
+    output: {
+      //filename: '[name].bundle.js',
+      filename: "[name].[chunkhash].js",
+      path: path.resolve(__dirname, 'dist')
+    },
+    target: "web",
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules)/,
+          resolve: { extensions: [".js",".jpg"] },
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              "plugins": ["@babel/plugin-transform-arrow-functions"]
+            }
           }
-        }
+        },
+        {
+          test: /\.(png|jp(e*)g|svg)$/,  
+          use: [{
+              loader: 'url-loader',
+              options: { 
+                  limit: 8000, // Convert images < 8kb to base64 strings
+                  name: 'images/[hash]-[name].[ext]'
+              } 
+          }]
       }
     ]
-    
-  },
-  plugins: [
-      new HtmlWebpackPlugin(),
-      new webpack.ProgressPlugin()
-  ],
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 3000
+      
+    },
+    plugins: [
+      new HtmlWebpackPlugin({  // Also generate a test.html
+        filename: 'index.html',
+        title: 'Learning Webpack'
+      }),
+        new webpack.ProgressPlugin(),
+        // new webpack.SourceMapDevToolPlugin({
+        //   filename: '[name].js.map',
+        //   exclude: ['vendor.js']
+        // })
+    ],
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 3000
+    }
   }
 };
